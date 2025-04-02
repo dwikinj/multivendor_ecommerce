ea08dfeccd5bccdfb58aabe82cddc480e4954bec
Dwiki Nur Jayanto <dwikinj@gmail.com>
2025-04-02 10:18:55 +0700

    add: basic stetup, shadcn ui, theme-provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+import ThemeToggle from "@/components/ui/shared/theme-toggle";
+
 export default function Home() {
   return (
-    <h1 className="font-bold text-red-500">Welcome</h1>
+    <div className="p-5">
+      <div className="flex justify-end">
+        <ThemeToggle />
+      </div>
+      <h1 className="font-bold text-blue-500 font-barlow text-8xl">Welcome</h1>
+      <Button variant={"destructive"}>Click Me</Button>
+    </div>
   );
 }
